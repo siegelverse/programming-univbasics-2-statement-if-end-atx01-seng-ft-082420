ecdcce50dda5df612466cc83ed025931da08d76e
Marcus Siegel <msiegel37@gmail.com>
2020-08-03 21:28:38 +0000

    Automatically backed up by Learn

--- learning-comments.rb
+++ learning-comments.rb
@@ -1,2 +1,10 @@
 chance_of_rain = 0.2 
-if chance_of_rain <=
+if chance_of_rain <= 0.25 
+  puts "Pack a sin shelter!"
+  elsif (chance_of_rain > 0.25 && 
+  chance_of_rain < 0.75)
+    puts "Pack an umbrella!"
+  else 
+    puts "Stay home and read Hegel."
+  end
+  
