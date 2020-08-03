57a3a406d429e9ec7c4d046c01ca7c6ad88f2899
Marcus Siegel <msiegel37@gmail.com>
2020-08-03 21:29:43 +0000

    Automatically backed up by Learn

--- learning-comments.rb
+++ learning-comments.rb
@@ -1,6 +1,6 @@
 chance_of_rain = 0.2 
 if chance_of_rain <= 0.25 
-  puts "Pack a sin shelter!"
+  puts "Pack a sun shelter!"
   elsif (chance_of_rain > 0.25 && 
   chance_of_rain < 0.75)
     puts "Pack an umbrella!"
